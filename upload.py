e236e265924731c799d5b38b604b4775c3c64503
Jianfeng Liu <liujianfeng1994@gmail.com>
2018-04-17 17:02:43 +0800

    Update upload.py

--- upload.py
+++ upload.py
@@ -14,8 +14,8 @@ class FileUploadHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
     def post(self):
-        ret = {'result': 'OK'}
-        upload_path = os.path.join(os.path.dirname(__file__), 'files')
+        ret = {'result': 'OK','url':''}
+        upload_path = os.path.join(os.path.dirname(__file__), 'static/files')
         file_metas = self.request.files.get('file', None)
 
         if not file_metas:
@@ -24,18 +24,28 @@ class FileUploadHandler(tornado.web.RequestHandler):
 
         for meta in file_metas:
             filename = meta['filename']
+            image_name, image_type = filename.split('.')
             file_path = os.path.join(upload_path, filename)
 
 
             with open(file_path, 'wb') as up:
                 up.write(meta['body'])
-                # OR do other thing
+
+            image_file = open(file_path,'rb')
+            image_file_data = image_file.read(4096)
+            image_file.close()
+            image_file_md5 = hashlib.md5(image_file_data)
+            image_file_md5value = image_file_md5.hexdigest()
+            new_image_name = image_file_md5value + '.' + image_type
+            image_path = os.path.join(upload_path,new_image_name)
+            shutil.move(file_path, image_path)
+            ret['url'] = 'http://10.90.79.233:8765/static/files/' + new_image_name
 
         self.write(json.dumps(ret))
 
 settings = {
-    'static_path': 'js',
-    'static_url_prefix': '/js/'
+    'static_path': 'static',
+    'static_url_prefix': '/static/'
 }
 
 app = tornado.web.Application([
